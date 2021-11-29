9f03e70dcfcf24d752f8484cfb36f703a6a24492
Alberto Ayala Rodriguez <alberto.ayala@newshore.es>
2021-11-28 23:15:47 -0500

    content done

--- src/app/team-selected/components/item-description/item-description.component.ts
+++ src/app/team-selected/components/item-description/item-description.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Params, ActivatedRoute } from '@angular/router';
+import { TeamsServices } from 'src/app/core/services/teams/teams.service';
+import { ResponseAPIGet, TeamCard } from '../../../typings'
 
 @Component({
   selector: 'app-item-description',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemDescriptionComponent implements OnInit {
 
-  constructor() { }
+  public currentItem!: string;
+  public currentIdTeam!: string;
+  public itemInformation!: ResponseAPIGet;
+  public team!: TeamCard;
+  public isAnArray!: boolean;
+  public noDescription: string = "it's doesn't have description :("
 
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+    private teamService: TeamsServices
+  ) { }
+
+  async ngOnInit(): Promise<void> {
+    this.setCurrentTeam();
+    await this.fetchItemById();
+    this.isAnArray = this.itemInformation?.flickr_images ? true : false;
   }
 
+  setCurrentTeam(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.currentItem = params.team;
+      this.currentIdTeam = params.id
+      this.team = this.teamService.getTeamByName(params.team);
+    });
+  }
+
+  async fetchItemById(): Promise<void> {
+    this.itemInformation = await this.teamService.getItemTeamDescriptionById(this.currentIdTeam, this.currentItem);
+  }
 }
--- src/app/team-selected/team-selected-routing.module.ts
+++ src/app/team-selected/team-selected-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ItemDescriptionComponent } from './components/item-description/item-description.component';
 import { TeamGaleryComponent } from './components/team-galery/team-galery.component';
-
 const routes: Routes = [
     {
         path: ':team',
